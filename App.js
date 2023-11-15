8ded6c88a120ba4d840eddb111d4d13fe6ae2826
enriqueAscencio <=>
2023-11-15 17:59:02 -0600

    calificar con estrellas temrinado

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, {useState} from 'react';
+
 
 export default function App() {
+  
+  const [defaultRating, setdefaultRaiting] = useState(2);
+  const [maxRaiting, setmaxRaiting] = useState([1,2,3,4,5]);
+  
+  
+  const starImgFilled  = require('./assets/star_filled.png');
+  const startImgCorner  = require('./assets/star_corner.png');
+
+  const CustomRatingBar = ()=>{
+    return(
+      <View style={styles.customRatingBarStyle}>
+        {
+          maxRaiting.map((item, key)=>{
+            return(
+              <TouchableOpacity
+              activeOpacity={0.7}
+              key={item}
+              onPress={()=> setdefaultRaiting(item)}>
+
+              <Image
+                style={styles.starImgStyle}
+                source={
+                  item <= defaultRating
+                  ? starImgFilled
+                  : startImgCorner
+                }
+              />
+
+              </TouchableOpacity>
+            );
+
+          })
+        }
+      </View>
+
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.textStyle}>Calificar!</Text>
+      <CustomRatingBar/>
+      <Text style={styles.textStyle}>
+        {defaultRating + ' / ' + maxRaiting.length}
+      </Text>
+      <TouchableOpacity
+        activeOpacity={0.7}
+        style={styles.buttonStyle}
+        onPress={()=>alert(defaultRating)}
+      >
+        <Text>Obtener valor</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle:{
+    textAlign:'center',
+    fontSize:23,
+    marginTop:20
+
+  },
+  customRatingBarStyle:{
+    justifyContent:'center',
+    flexDirection: 'row',
+    marginTop:30
+  },
+  starImgStyle:{
+    width:40,
+    height:40,
+    resizeMode:'cover'
+  },
+  buttonStyle:{
+    justifyContent:'center',
+    alignItems:'center',
+    marginTop:30,
+    padding:15,
+    backgroundColor:'green'
+  }
 });
